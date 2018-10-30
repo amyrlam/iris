136bb1c77e1ddd0edc61feb360d75a539ea48ed9
John Cowen <jcowen@hashicorp.com>
2018-10-30 10:58:01 +0000

    Tweaking the icons

--- test/storybook/loader.js
+++ test/storybook/loader.js
@@ -1,4 +1,5 @@
 const parse5 = require('parse5');
+const domWalk = require('dom-walk');
 const postcss = require('postcss');
 const loadCss = require('postcss-import/lib/load-content');
 const TemplateFactory = require('@gardenhq/tick-control');
@@ -7,37 +8,63 @@ const Template = TemplateFactory();
 const template = function(str) {
   return new Template(str).render();
 }
+const flatten = function(source, from, add) {
+  return postcss(
+    [
+      require('postcss-import')(
+        {
+          load: function(file) {
+            add(file);
+            return loadCss.apply(null, arguments);
+          }
+        }
+      )
+    ]
+  ).process(
+    source,
+    {
+      from: from,
+    }
+  );
+}
+const getElementsByTagName = function(tree, tagName) {
+  tagName = tagName.toLowerCase();
+  const els = [];
+  domWalk(
+    tree.childNodes,
+    function (node) {
+      if (node.tagName && (tagName === '*' || node.tagName.toLowerCase() === tagName)) {
+        els.push(node);
+      }
+    }
+  );
+  return els;
+}
 
 module.exports = function (source) {
   const cb = this.async();
   const htmlFilePath = this.resourcePath;
   const parsed = parse5.parse(template(source));
   const o = this;
-  const style = parsed.childNodes.find(
-    function(child) {
-      return child.nodeName === 'html';
+  let $styles = getElementsByTagName(parsed, 'style');
+  const style = $styles.reduce(
+    function(prev, item) {
+      const style = item.childNodes[0].value;
+      item.childNodes[0].value = '';
+      return `${prev}
+${style}`
+    },
+    ''
+  );
+  flatten(style, htmlFilePath, o.addDependency.bind(this)).then(
+    function(result) {
+      // console.log(result);
+      return result.css;
     }
-  ).childNodes.find(
-    function(child) {
-      return child.nodeName === 'head';
-    }
-  ).childNodes.filter(
-    function(child) {
-      return child.nodeName === 'style';
-    }
-  ).map(
-    function(style) {
-      const source = style.childNodes[0].value;
+  ).then(
+    function(source) {
       return postcss(
         [
-          require('postcss-import')(
-            {
-              load: function(file) {
-                o.addDependency(file);
-                return loadCss.apply(null, arguments);
-              }
-            }
-          ),
           require('postcss-extend')()
         ]
       ).process(
@@ -45,17 +72,15 @@ module.exports = function (source) {
         {
           from: htmlFilePath,
         }
-      ).then(
-        function(result) {
-          style.childNodes[0].value = result.css;
-          return style;
-        }
       );
     }
-  );
-  Promise.all(style).then(
-    function() {
-      cb(null, parse5.serialize(parsed))
+  ).then(
+    function(result) {
+      return result.css.toString()
+    }
+  ).then(
+    function(css) {
+      cb(null, '<link href="data:text/css;base64,' +Buffer(css).toString('base64') +  '" rel="stylesheet" type="text/css" />' + parse5.serialize(parsed));
     }
   );
 };
--- test/storybook/webpack.config.js
+++ test/storybook/webpack.config.js
@@ -1,5 +1,4 @@
 const path = require('path');
-const ExtraWatchWebpackPlugin = require('extra-watch-webpack-plugin');
 
 module.exports = (storybookBaseConfig, configType, defaultConfig) => {
   defaultConfig.module.rules[2].use = [
