52f14179062403c5f7b35af6513331bbf5959433
John Cowen <jcowen@hashicorp.com>
2019-05-10 15:53:17 +0000

    Continue adding in more components

--- test/stories/component.stories.js
+++ test/stories/component.stories.js
@@ -1,11 +1,21 @@
 import { document, console } from 'global';
 import { storiesOf } from '@storybook/html';
+import centered from '@storybook/addon-centered/html';
 import buttons from '../fixtures/components/buttons.html';
 import notice from '../fixtures/components/notice.html';
 import pill from '../fixtures/components/pill.html';
 import tooltip from '../fixtures/components/tooltip.html';
+import actionGroup from '../fixtures/components/action-group.html';
+import toggleButton from '../fixtures/components/toggle-button.html';
+// potentially shouldn't be in components?
+import table from '../fixtures/components/table.html';
+
 storiesOf('Components', module)
+  .addDecorator(centered)
+  .add('ActionGroup', () => actionGroup)
   .add('Buttons', () => buttons)
+  .add('ToggleButton', () => toggleButton)
+  .add('Table', () => table)
   .add('Notice', () => notice)
   .add('Pill', () => pill)
   .add('Tooltip', () => tooltip);
