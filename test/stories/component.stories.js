b56fe388d6e0b8a225acee096497b29d6891747b
John Cowen <jcowen@hashicorp.com>
2019-05-31 15:49:51 +0000

    Anchors, table, breadcrumbs, tabs

--- test/stories/component.stories.js
+++ test/stories/component.stories.js
@@ -7,15 +7,21 @@ import pill from '../fixtures/components/pill.html';
 import tooltip from '../fixtures/components/tooltip.html';
 import actionGroup from '../fixtures/components/action-group.html';
 import toggleButton from '../fixtures/components/toggle-button.html';
+import breadcrumbs from '../fixtures/components/breadcrumbs.html';
+import tabs from '../fixtures/components/tabs.html';
 // potentially shouldn't be in components?
 import table from '../fixtures/components/table.html';
+import anchors from '../fixtures/components/anchors.html';
 
 storiesOf('Components', module)
   .addDecorator(centered)
+  .add('Anchors', () => anchors)
   .add('ActionGroup', () => actionGroup)
   .add('Buttons', () => buttons)
+  .add('Breadcrumbs', () => breadcrumbs)
   .add('ToggleButton', () => toggleButton)
   .add('Table', () => table)
+  .add('Tabs', () => tabs)
   .add('Notice', () => notice)
   .add('Pill', () => pill)
   .add('Tooltip', () => tooltip);
