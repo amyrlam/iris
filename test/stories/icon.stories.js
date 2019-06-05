0f0e332fb6dc4c2dbcccbfafce0c6832fcc97eb9
John Cowen <jcowen@hashicorp.com>
2019-06-05 09:49:18 +0000

    Pull in @hashicorp/structure-icons

--- test/stories/icon.stories.js
+++ test/stories/icon.stories.js
@@ -1,6 +1,6 @@
 import { document, console } from 'global';
 import { storiesOf } from '@storybook/html';
-import icons from '../fixtures/icons.html';
+import icons from '../fixtures/icons/backgrounds.html';
 storiesOf('Icons', module)
   .add('CSS Backgrounds', () => icons)
   .add('Inline HTML', () => icons);
